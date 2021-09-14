64ec2dc4ee61074acca5ef9ccba12426e7814eea
Tatyanenko Alexey <alexdtat@gmail.com>
2021-09-14 15:46:03 +0300

    Solution of the first task added.

---
+++ homework1/task_1.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <stdio.h>
+
+void divisionWithRemainder(int a, int b)
+{
+    int quotient = 0;
+    int remainder = 0;
+    int temporaryA = abs(a);
+    int temporaryB = abs(b);
+
+    while (temporaryA >= temporaryB) {
+        temporaryA -= temporaryB;
+        ++quotient;
+    }
+    if (a < 0)
+        quotient = -quotient - 1;
+    if (b < 0)
+        quotient = -quotient;
+    remainder = a - quotient * b;
+
+    printf("%d %c %d %c %d %c %d %c", a, '/', b, '=', quotient, '(', remainder, ')');
+}
+
+int main(int argc, char *argv[])
+{
+    int numberA = 0;
+    int numberB = 0;
+
+    printf("%s", "Please, enter the number a:\n");
+    scanf("%d", &numberA);
+    printf("%s", "Please, enter the number b:\n");
+    scanf("%d", &numberB);
+
+    divisionWithRemainder(numberA, numberB);
+    return 0;
+}
