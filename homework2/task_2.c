67031584b999c8a35b057be2f78ea19be78a05ba
Tatyanenko Alexey <alexdtat@gmail.com>
2021-10-10 16:44:06 +0300

    Multiple defects were eliminated.

--- homework2/task_2.c
+++ homework2/task_2.c
@@ -5,77 +5,72 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SEQUENCE_DOES_NOT_OCCUR NULL
+#define DELETION_ERROR -1
+#define INSERTION_ERROR -2
+#define REPLACEMENT_ERROR -3
 
-DoublyLinkedListElement* findFirstOccurrenceSince(const char* searchedSequence, DoublyLinkedListElement* start)
+bool find(DoublyLinkedListElement* start, const char* sequence, DoublyLinkedListElement** matchStart, DoublyLinkedListElement** matchEnd)
 {
     DoublyLinkedListElement* elementOfSequence = NULL;
-    bool isSequenceBeginning = true;
+    int sequenceLength = strlen(sequence);
+
+    if (!strlen(sequence))
+        return start;
 
     for (DoublyLinkedListElement* currentElement = start; currentElement; currentElement = getNextElement(currentElement)) {
-        if (getSymbol(currentElement) == searchedSequence[0]) {
-            isSequenceBeginning = true;
-            elementOfSequence = currentElement;
-            for (int i = 0; i < strlen(searchedSequence) && elementOfSequence && isSequenceBeginning; i++) {
-                if (getSymbol(elementOfSequence) != searchedSequence[i])
-                    isSequenceBeginning = false;
-                if ((i == strlen(searchedSequence) - 1) && isSequenceBeginning)
-                    return currentElement;
-                elementOfSequence = getNextElement(elementOfSequence);
+        elementOfSequence = currentElement;
+        for (int i = 0; elementOfSequence && getSymbol(elementOfSequence) == sequence[i]; i++) {
+            if (i == sequenceLength - 1) {
+                *matchStart = currentElement;
+                *matchEnd = elementOfSequence;
+                return true;
             }
+            elementOfSequence = getNextElement(elementOfSequence);
         }
     }
 
-    return SEQUENCE_DOES_NOT_OCCUR;
+    return false;
 }
 
-DoublyLinkedListElement* goToEndOfFirstOccurrenceSince(const char* sequence, DoublyLinkedListElement* start)
-{
-    DoublyLinkedListElement* currentPosition = NULL;
-
-    currentPosition = findFirstOccurrenceSince(sequence, start);
-    if (!currentPosition)
-        return SEQUENCE_DOES_NOT_OCCUR;
-    for (int i = 0; i < strlen(sequence) - 1; i++)
-        currentPosition = getNextElement(currentPosition);
-
-    return currentPosition;
-}
 
 void insertFragmentAfterElement(DoublyLinkedList* list, DoublyLinkedListElement* elementBefore, const char* fragment)
 {
     DoublyLinkedListElement* currentPosition = elementBefore;
+    int fragmentLength = strlen(fragment);
 
-    for (int i = 0; i < strlen(fragment); i++) {
+    for (int i = 0; i < fragmentLength; i++) {
         insertElementAfter(list, createNewElement(fragment[i]), currentPosition);
         currentPosition = getNextElement(currentPosition);
     }
 }
 
-void deleteFragmentInRange(DoublyLinkedList* list, DoublyLinkedListElement* startingElement, DoublyLinkedListElement* elementAfter)
+void deleteFragmentInRange(DoublyLinkedList* list, DoublyLinkedListElement* start, DoublyLinkedListElement* end)
 {
-    for (DoublyLinkedListElement* currentPosition = startingElement; currentPosition != elementAfter; currentPosition = deleteElementAndGetNext(list, currentPosition)) { }
+    for (DoublyLinkedListElement* current = start; current != end; current = deleteElementAndGetNext(list, current)) { }
 }
 
 bool insertFragment(DoublyLinkedList* list, const char* start, const char* fragment)
 {
-    DoublyLinkedListElement* currentPosition = goToEndOfFirstOccurrenceSince(start, getHead(list));
-    if (!currentPosition)
+    DoublyLinkedListElement* startingPosition = NULL;
+    DoublyLinkedListElement* endingPosition = NULL;
+    if (!find(getHead(list), start, &startingPosition, &endingPosition))
         return false;
 
-    insertFragmentAfterElement(list, currentPosition, fragment);
+    insertFragmentAfterElement(list, endingPosition, fragment);
 
     return true;
 }
 
 bool deleteFragment(DoublyLinkedList* list, const char* start, const char* end)
 {
-    DoublyLinkedListElement* startingPosition = findFirstOccurrenceSince(start, getHead(list));
-    if (!startingPosition)
+    DoublyLinkedListElement* startingPosition = NULL;
+    DoublyLinkedListElement* startSequenceLastElement = NULL;
+    if (!find(getHead(list), start, &startingPosition, &startSequenceLastElement))
         return false;
 
-    DoublyLinkedListElement* lastElement = goToEndOfFirstOccurrenceSince(end, getNextElement(goToEndOfFirstOccurrenceSince(start, getHead(list))));
-    if (!lastElement)
+    DoublyLinkedListElement* endSequenceFirstElement = NULL;
+    DoublyLinkedListElement* lastElement = NULL;
+    if (!find(getNextElement(startSequenceLastElement), end, &endSequenceFirstElement, &lastElement))
         return false;
     DoublyLinkedListElement* endingPosition = getNextElement(lastElement);
 
@@ -85,13 +80,12 @@ bool deleteFragment(DoublyLinkedList* list, const char* start, const char* end)
 
 bool replaceFragment(DoublyLinkedList* list, const char* template, const char* fragment)
 {
-    DoublyLinkedListElement* startingPosition = findFirstOccurrenceSince(template, getHead(list));
-    if (!startingPosition)
+    DoublyLinkedListElement* startingPosition = NULL;
+    DoublyLinkedListElement* lastElement = NULL;
+    if (!find(getHead(list), template, &startingPosition, &lastElement))
         return false;
+
     DoublyLinkedListElement* elementBeforeStart = getPreviousElement(startingPosition);
-    DoublyLinkedListElement* lastElement = goToEndOfFirstOccurrenceSince(template, getHead(list));
-    if (!lastElement)
-        return false;
     DoublyLinkedListElement* endingPosition = getNextElement(lastElement);
 
     deleteFragmentInRange(list, startingPosition, endingPosition);
@@ -100,10 +94,8 @@ bool replaceFragment(DoublyLinkedList* list, const char* template, const char* f
     return true;
 }
 
-void readFileIntoDoublyLinkedListAndWriteIntoOther(const char* sourcesFilePath, const char* resultsFilePath, DoublyLinkedList* sequence)
+int recoverDNA(const char* sourcesFilePath, const char* resultsFilePath)
 {
-    FILE* sourcesFile = NULL;
-    FILE* resultsFile = NULL;
     char currentCommand[8];
     char firstArgument[128];
     char secondArgument[128];
@@ -111,64 +103,68 @@ void readFileIntoDoublyLinkedListAndWriteIntoOther(const char* sourcesFilePath, 
     int numberOfLines = 0;
     errno = 0;
 
-    sourcesFile = fopen(sourcesFilePath, "r");
+    FILE* sourcesFile = fopen(sourcesFilePath, "r");
+    if (!sourcesFile)
+        return errno;
 
-    if (sourcesFile) {
-        resultsFile = fopen(resultsFilePath, "w");
-        if (resultsFile) {
-            fscanf(sourcesFile, "%d\n", &sequenceSize);
-            for (int i = 0; i < sequenceSize; i++) {
-                appendElement(sequence, createNewElement(fgetc(sourcesFile)));
-            }
-            fscanf(sourcesFile, "%d\n", &numberOfLines);
-            for (int i = 0; i < numberOfLines; i++) {
-                fscanf(sourcesFile, "%s %s %s\n", currentCommand, firstArgument, secondArgument);
-                if (strcmp(currentCommand, "INSERT") == 0 && !insertFragment(sequence, firstArgument, secondArgument)) {
-                    printf("INSERTION ERROR!\n");
-                    fclose(resultsFile);
-                    fclose(sourcesFile);
-                    return;
-                }
-                if (strcmp(currentCommand, "REPLACE") == 0 && !replaceFragment(sequence, firstArgument, secondArgument)) {
-                    printf("REPLACEMENT ERROR!\n");
-                    fclose(resultsFile);
-                    fclose(sourcesFile);
-                    return;
-                }
-                if (strcmp(currentCommand, "DELETE") == 0 && !deleteFragment(sequence, firstArgument, secondArgument)) {
-                    printf("DELETION ERROR!\n");
-                    fclose(resultsFile);
-                    fclose(sourcesFile);
-                    return;
-                }
-                for (DoublyLinkedListElement* currentElement = getHead(sequence); currentElement; currentElement = getNextElement(currentElement)) {
-                    fprintf(resultsFile, "%c", getSymbol(currentElement));
-                }
-                fprintf(resultsFile, "\n");
-            }
-
-            fclose(resultsFile);
-        } else
-            printf("RESULTS FILE OPENING ERROR! The error's code (errno):\t%d\n", errno);
-
+    FILE* resultsFile = fopen(resultsFilePath, "w");
+    if (!resultsFile) {
         fclose(sourcesFile);
-    } else
-        printf("SOURCES FILE OPENING ERROR! The error's code (errno):\t%d\n", errno);
+        return errno;
+    }
+
+    DoublyLinkedList* sequence = createNewDoublyLinkedList();
+
+    fscanf(sourcesFile, "%d\n", &sequenceSize);
+    for (int i = 0; i < sequenceSize; i++) {
+        appendElement(sequence, createNewElement(fgetc(sourcesFile)));
+    }
+    fscanf(sourcesFile, "%d\n", &numberOfLines);
+    for (int i = 0; i < numberOfLines; i++) {
+        fscanf(sourcesFile, "%s %s %s\n", currentCommand, firstArgument, secondArgument);
+        if (strcmp(currentCommand, "INSERT") == 0 && !insertFragment(sequence, firstArgument, secondArgument)) {
+            fclose(resultsFile);
+            fclose(sourcesFile);
+            deleteDoublyLinkedList(sequence);
+            return INSERTION_ERROR;
+        }
+        if (strcmp(currentCommand, "REPLACE") == 0 && !replaceFragment(sequence, firstArgument, secondArgument)) {
+            fclose(resultsFile);
+            fclose(sourcesFile);
+            deleteDoublyLinkedList(sequence);
+            return REPLACEMENT_ERROR;
+        }
+        if (strcmp(currentCommand, "DELETE") == 0 && !deleteFragment(sequence, firstArgument, secondArgument)) {
+            fclose(resultsFile);
+            fclose(sourcesFile);
+            deleteDoublyLinkedList(sequence);
+            return DELETION_ERROR;
+        }
+        for (DoublyLinkedListElement* currentElement = getHead(sequence); currentElement; currentElement = getNextElement(currentElement)) {
+            fprintf(resultsFile, "%c", getSymbol(currentElement));
+        }
+        fprintf(resultsFile, "\n");
+    }
+
+    fclose(resultsFile);
+    fclose(sourcesFile);
+    deleteDoublyLinkedList(sequence);
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc == 3) {
-        DoublyLinkedList* sequence = NULL;
-        sequence = createNewDoublyLinkedList();
-        char sourcesFilePath[256];
-        char resultsFilePath[256];
-        strcpy(sourcesFilePath, argv[1]);
-        strcpy(resultsFilePath, argv[2]);
-
-        readFileIntoDoublyLinkedListAndWriteIntoOther(sourcesFilePath, resultsFilePath, sequence);
-
-        deleteDoublyLinkedList(sequence);
-    } else
+    if (argc != 3) {
         printf("CONSOLE INPUT FORMAT VIOLATION!");
+        return 0;
+    }
+    int resultCode = recoverDNA(argv[1], argv[2]);
+    if (resultCode > 0)
+        printf("FILE OPENING ERROR! The error's code (errno):\t%d\n", errno);
+    if (resultCode == DELETION_ERROR)
+        printf("DELETION ERROR!\n");
+    if (resultCode == INSERTION_ERROR)
+        printf("INSERTION ERROR!\n");
+    if (resultCode == REPLACEMENT_ERROR)
+        printf("REPLACEMENT ERROR!\n");
 }
